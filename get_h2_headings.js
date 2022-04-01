c2e7c749eeab9e22c2f1a308e4910d34f16b7109
Christian Hänsel <chris@haensel.pro>
2022-04-01 09:26:16 +0200

    Added extraction for h1 and h2 with their XPath

---
+++ get_h2_headings.js
@@ -0,0 +1,51 @@
+/**
+ * @author Christan Hänsel
+ * 
+ * gets the h2 heading(s) and XPath and displays them in the extended table
+ */
+function createXPathFromElement(elm) {
+    var allNodes = document.getElementsByTagName('*');
+    for (var segs = []; elm && elm.nodeType == 1; elm = elm.parentNode) {
+        if (elm.hasAttribute('id')) {
+            var uniqueIdCount = 0;
+            for (var n = 0; n < allNodes.length; n++) {
+                if (allNodes[n].hasAttribute('id') && allNodes[n].id == elm.id) uniqueIdCount++;
+                if (uniqueIdCount > 1) break;
+            };
+            if (uniqueIdCount == 1) {
+                segs.unshift('id("' + elm.getAttribute('id') + '")');
+                return segs.join('/');
+            } else {
+                segs.unshift(elm.localName.toLowerCase() + '[@id="' + elm.getAttribute('id') + '"]');
+            }
+        } else if (elm.hasAttribute('class')) {
+            segs.unshift(elm.localName.toLowerCase() + '[@class="' + elm.getAttribute('class') + '"]');
+        } else {
+            for (i = 1, sib = elm.previousSibling; sib; sib = sib.previousSibling) {
+                if (sib.localName == elm.localName) i++;
+            };
+            segs.unshift(elm.localName.toLowerCase() + '[' + i + ']');
+        };
+    };
+    return segs.length ? '/' + segs.join('/') : null;
+};
+
+
+/* Retrieve some information from the page */
+const data = document.querySelectorAll('h2')
+
+
+
+/* Map it into an array */
+const extended = [...data].map(e => [createXPathFromElement(e), e.innerText])
+
+/* Add the header row */
+extended.unshift(['XPath', 'Content'])
+
+const ret = {
+    cell: data.length,
+    /* The data to be displayed in the cell */
+    extended: extended /* The explore table when the cell is clicked */
+}
+
+return ret
